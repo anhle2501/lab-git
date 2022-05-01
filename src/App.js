a565735ed0d2de1403a6bdb3c7708680e0593636
nhut.anh2501@gmail.com <nhut.anh2501@gmail.com>
2022-05-01 22:47:25 +0700

    test amend them file test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render (){
     return (
       <div className="App">
-        Hello World 1 3 2 4 5 6
+        Hello World 1 3 2 4 5 6 7
       </div> 
     )
   }
